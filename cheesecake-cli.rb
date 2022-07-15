bb8bdbd5e4a7d7d792ddd6c8ee0708b7951c5913
Paulo Real <paulo.real@ckl.io>
2022-07-15 02:31:47 -0300

    feat: add homebrew formula file

---
+++ cheesecake-cli.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CheesecakeCli < Formula
+  desc "CLI for new projects"
+  homepage ""
+  url "https://github.com/CheesecakeLabs/cheesecake-cli/archive/refs/tags/v0.1.tar.gz"
+  sha256 "83dd4d2c72d10175511df6602727717e54e9f6b0b92e6a0f37cc3a6ac21e2a1b"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cheesecake-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
